6b6d2ff620784e1745799c72bd1ae2aa95a89512
Mark Geller <gellmark@gmail.com>
2015-07-23 18:32:28 +0300

    As u wish

--- src/Common/Handle.java
+++ src/Common/Handle.java
@@ -1,18 +1,20 @@
 package Common;
 
+import java.net.URL;
+
 /**
  * Created by user on 7/23/15.
  *
  */
 public class Handle {
-    public final String URL;
+    public final URL url;
 
     public final String XPATH;
 
     public int elType;
 
-    public Handle(String url, String xpath, int eltype) {
-        URL = url;
+    public Handle(URL iurl, String xpath, int eltype) {
+        url = iurl;
         XPATH = xpath;
         elType = eltype;
     }
---
+++ src/Common/State.java
@@ -0,0 +1,10 @@
+package Common;
+
+import java.net.URL;
+
+/**
+ * Created by user on 7/23/15.
+ */
+public class State {
+    URL url;
+}
