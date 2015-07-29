86df1c3acbc53cab3927946d2ec2d20207f4c748
listvin <fyodor-listvin@ya.ru>
2015-07-29 19:37:52 +0300

    Added imlicit waits instead of explicit. One explicit wait left in checkHandleType(...) in cycle. It can freeze here now. =/

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,5 +1,7 @@
+import Boxes.Sequence;
+import Boxes.State;
+import Boxes.WebHandle;
 import Common.*;
-import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -7,26 +9,29 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by user on 7/23/15.
  */
 public class Scanner {
     private WebDriver driver;
+    private Alphabet alphabet;
 
     /** Constructs Scanner by creating a Firefox (at the moment) driver and logging at "http://localhost:8080/login" with root/root*/
     public Scanner(){
-        this.driver = new FirefoxDriver();
-        //TODO ACHTUNG THIS SHOULD NOT EXIST!!!!
-        driver.get("http://localhost:8080/login");
-        try {
-            Thread.sleep(500);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-        System.out.print(By.cssSelector("input"));
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(2, TimeUnit.SECONDS);
+
+        alphabet = new MapAlphabet();
+
+        //TODO ACHTUNG THIS (hardcoded login) SHOULD NOT EXIST!!!!
+        driver.get("http://localhost:8080/login"); //#hardcode
+        System.err.printf("Have logged in with root/root at\n\tlocalhost:8080/login\n");
+
         driver.findElement(By.id("id_l.L.login")).sendKeys("root");
         driver.findElement(By.id("id_l.L.password")).sendKeys("root");
         driver.findElement(By.id("id_l.L.loginButton")).click();
@@ -44,12 +49,26 @@ public class Scanner {
      * @return ElementType. unknown if driver url didnt match handle
      * @throws NoSuchElementException when element cant be found by its xpath
      */
-    //TODO More precise check for writable type. Mb separate?
-    public static ElementType checkHandleType(WebDriver driver, WebHandle handle) throws NoSuchElementException{
+    public ElementType checkHandleType(WebDriver driver, WebHandle handle) throws NoSuchElementException{
         try {
             //###### Verifying opened url
-            if (!driver.getCurrentUrl().equals(handle.url.toString()))
-                throw new WebDriverException("URL opened in browser is not corresponding to base URL in state");
+            //TODO remove this crutch
+            if (!driver.getCurrentUrl().equals(handle.url.toString())){
+                do {
+                    System.err.println("opened URL: " + driver.getCurrentUrl());
+                    System.err.println("handle URL: " + handle.url.toString());
+                    System.err.println("=======================TRYING TO SLEEP NOW=======================");
+                    Utils.sleep(1000);
+                }while(!driver.getCurrentUrl().equals(handle.url.toString()));
+//                if (!driver.getCurrentUrl().equals(handle.url.toString())){
+//                    throw new WebDriverException("URL opened in browser is not corresponding to URL in WebHandle");
+//                }
+            }
+
+            //###### Checking for cached result
+            ElementType cachedEltype = alphabet.request(handle.url, handle.xpath);
+            if (cachedEltype != ElementType.unknown)
+                return cachedEltype;
 
             //###### Storing data before tries to interact
             String oldHash = Utils.hashPage(driver);
@@ -103,6 +122,7 @@ public class Scanner {
 
             //###### Checking writability
             //TODO return writability check instead of simple clicker check above
+            //TODO More precise check for writable type. Mb separate?
             /*if (driver.findElements(By.xpath(handle.xpath)).size() > 0) {
                 String oldValue = handle.findElement(driver).getAttribute("value");
                 handle.findElement(driver).sendKeys("aba");
@@ -142,48 +162,55 @@ public class Scanner {
      * @return - list of all interactable items
      */
     public List<WebHandle> scan(State baseState){
-        baseState.reach(driver);
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
         List<WebHandle> interactiveHandles = new ArrayList<>();
         List<WebHandle> allHandles = new ArrayList<>();
 
+        baseState.reach(driver);
+
+        List<WebElement> elementList = driver.findElements(By.cssSelector("*"));
+        System.err.printf("scan() of Scanner invoked. baseState.\n" +
+                        "\t.url : %s\n" +
+                        "\tsequence.size() : %d\n" +
+                        "\tFound %d elements. Started generating xpathes...\n",
+                baseState.url.toString(), baseState.sequence.size(), elementList.size());
+
+        //TODO remove threshold for count of generated xpathes per scan session
+        int i = 0, xpathsThreshold = 80; //#hardcode
+        URL curUrl = null;
+        try {
+            curUrl = new URL(driver.getCurrentUrl());
+        } catch (MalformedURLException e) {
+            //This MalformedURLException is rather annoying.
+            //Why at all we still use the URL class...
+            e.printStackTrace();
+        }
+        for(WebElement element: elementList){
+            if(!element.isDisplayed() || !element.isEnabled()) continue;
+            String xpath = Selectors.formXPATH(driver, element);
+            WebHandle handle = new WebHandle(curUrl, xpath);
+            allHandles.add(handle);
+            if (++i > xpathsThreshold) break;
+        }
+
+        for (WebHandle handle: allHandles){
             baseState.reach(driver);
-
-            List<WebElement> el = driver.findElements(By.cssSelector("*"));
-            System.out.println(el.size());
-
-            for(WebElement element: el){
-                if(!element.isDisplayed() || !element.isEnabled()){
-                    continue;
-                }
-                String xpath = Selectors.formXPATH(driver, element);
-                WebHandle handle = new WebHandle(baseState.url, xpath);
-                allHandles.add(handle);
+//            System.err.println("XPATH for element being tested: " + handle.xpath);
+            try {
+                handle.eltype = checkHandleType(driver, handle);
+            }catch (NoSuchElementException e){
+                //TODO: after hovering over button tooltip appears changing page structure(Extra div) => couldn't handle element.
+                continue;
             }
 
-            for(WebHandle handle: allHandles){
-                baseState.reach(driver);
-                System.out.println("XPATH for element: " + handle.xpath);
-                try {
-                    handle.eltype = checkHandleType(driver, handle);
-                }catch (NoSuchElementException e){
-                    //TODO: after hovering over button tolltip appears chainging page structure(Extra div) => couldn't handle element.
-                    continue;
-                }
-
-                System.out.println("Determined as " + handle.eltype.name() + "\n");
-                //TODO return writables
+//            System.err.println("Determined as " + handle.eltype.name() + "\n");
+            //TODO return writables
 //                if(handle.eltype.equals(ElementType.clickable) || handle.eltype.equals(ElementType.writable)) {
-                if(handle.eltype.equals(ElementType.clickable)) {
-                    interactiveHandles.add(handle);
-                    //TODO remove intaractive elements count limit
-                    if (interactiveHandles.size() >= 20) return interactiveHandles;
-                }
+            if(handle.eltype.equals(ElementType.clickable)) {
+                interactiveHandles.add(handle);
+                //TODO remove intaractive elements count limit
+                if (interactiveHandles.size() >= 20) return interactiveHandles; //#hardcode
             }
+        }
         return interactiveHandles;
     }
 
